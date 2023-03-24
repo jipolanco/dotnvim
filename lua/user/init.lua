c6640bd78212800c1a4324de6091b932c67ee42a
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-03-24 15:39:34 +0100

    Update config to Astronvim 3

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,137 +1,25 @@
---              AstroNvim Configuration Table
--- All configuration changes should go inside of the table below
-
--- You can think of a Lua "table" as a dictionary like data structure the
--- normal format is "key = value". These also handle array like data structures
--- where a value with no key simply has an implicit numeric key
-local config = {
-
+return {
   -- Configure AstroNvim updates
   updater = {
     remote = "origin", -- remote to use
     channel = "stable", -- "stable" or "nightly"
     version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "main", -- branch name (NIGHTLY ONLY)
+    branch = "nightly", -- branch name (NIGHTLY ONLY)
     commit = nil, -- commit hash (NIGHTLY ONLY)
     pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
     skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_reload = false, -- automatically reload and sync packer after a successful update
     auto_quit = false, -- automatically quit the current session after a successful update
-    -- remotes = { -- easily add new remotes to track
-    --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-    --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-    --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    -- },
+    remotes = { -- easily add new remotes to track
+      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
+      --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
+      --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
+    },
   },
 
   -- Set colorscheme to use
-  colorscheme = "default_theme",
+  colorscheme = "astrodark",
   -- colorscheme = "nightfox",
-
-  -- Add highlight groups in any theme
-  highlights = {
-    -- init = { -- this table overrides highlights in all themes
-    --   Normal = { bg = "#000000" },
-    -- }
-    -- duskfox = { -- a table of overrides/changes to the duskfox theme
-    --   Normal = { bg = "#000000" },
-    -- },
-  },
-
-  -- set vim options here (vim.<first_key>.<second_key> = value)
-  options = {
-    opt = {
-      -- set to true or false etc.
-      relativenumber = true, -- sets vim.opt.relativenumber
-      number = true, -- sets vim.opt.number
-      spell = false, -- sets vim.opt.spell
-      signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = true, -- sets vim.opt.wrap
-      splitbelow = false,
-      splitright = true,
-      shell = "/bin/bash", -- avoid using fish (very slow)
-      -- showcmd = true,
-    },
-    g = {
-      mapleader = " ", -- sets vim.g.mapleader
-      autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-      cmp_enabled = true, -- enable completion at start
-      autopairs_enabled = true, -- enable autopairs at start
-      diagnostics_enabled = true, -- enable diagnostics at start
-      status_diagnostics_enabled = true, -- enable diagnostics in statusline
-      icons_enabled = false, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-      ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-    },
-  },
-  -- If you need more control, you can use the function()...end notation
-  -- options = function(local_vim)
-  --   local_vim.opt.relativenumber = true
-  --   local_vim.g.mapleader = " "
-  --   local_vim.opt.whichwrap = vim.opt.whichwrap - { 'b', 's' } -- removing option from list
-  --   local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
-  --
-  --   return local_vim
-  -- end,
-
-  -- Set dashboard header
-  header = {
-    " █████  ███████ ████████ ██████   ██████",
-    "██   ██ ██         ██    ██   ██ ██    ██",
-    "███████ ███████    ██    ██████  ██    ██",
-    "██   ██      ██    ██    ██   ██ ██    ██",
-    "██   ██ ███████    ██    ██   ██  ██████",
-    " ",
-    "    ███    ██ ██    ██ ██ ███    ███",
-    "    ████   ██ ██    ██ ██ ████  ████",
-    "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-    "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-    "    ██   ████   ████   ██ ██      ██",
-  },
-
-  -- Default theme configuration
-  default_theme = {
-    -- Modify the color palette for the default theme
-    colors = {
-      fg = "#abb2bf",
-      bg = "#1e222a",
-    },
-    highlights = function(hl) -- or a function that returns a new table of colors to set
-      local C = require "default_theme.colors"
-
-      hl.Normal = { fg = C.fg, bg = C.bg }
-
-      -- New approach instead of diagnostic_style
-      hl.DiagnosticError.italic = true
-      hl.DiagnosticHint.italic = true
-      hl.DiagnosticInfo.italic = true
-      hl.DiagnosticWarn.italic = true
-
-      return hl
-    end,
-    -- enable or disable highlighting for extra plugins
-    plugins = {
-      aerial = true,
-      beacon = false,
-      bufferline = true,
-      cmp = true,
-      dashboard = true,
-      highlighturl = true,
-      hop = false,
-      indent_blankline = true,
-      lightspeed = false,
-      ["neo-tree"] = true,
-      notify = true,
-      ["nvim-tree"] = false,
-      ["nvim-web-devicons"] = true,
-      rainbow = true,
-      symbols_outline = false,
-      telescope = true,
-      treesitter = true,
-      vimwiki = false,
-      ["which-key"] = true,
-    },
-  },
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
@@ -139,15 +27,8 @@ local config = {
     underline = true,
   },
 
-  -- Extend LSP configuration
   lsp = {
-    -- enable servers that you already have installed without mason
-    servers = {
-      "julials",
-      "texlab",
-      "ccls",
-      -- "pyright"
-    },
+    -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
@@ -167,20 +48,13 @@ local config = {
       --   return true
       -- end
     },
-    -- easily add or disable built in mappings added during LSP attaching
-    mappings = {
-      n = {
-        -- ["<leader>lf"] = false -- disable formatting keymap
-      },
+    -- enable servers that you already have installed without mason
+    servers = {
+      "julials",
+      "texlab",
+      "ccls",
+      -- "pyright"
     },
-    -- add to the global LSP on_attach function
-    -- on_attach = function(client, bufnr)
-    -- end,
-
-    -- override the mason server-registration function
-    -- server_registration = function(server, opts)
-    --   require("lspconfig")[server].setup(opts)
-    -- end,
 
     -- Add overrides for LSP server settings, the keys are the name of the server
     ["server-settings"] = {
@@ -212,216 +86,16 @@ local config = {
               },
           }
       },
-      -- example for addings schemas to yamlls
-      -- yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
-      --   settings = {
-      --     yaml = {
-      --       schemas = {
-      --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-      --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-      --       },
-      --     },
-      --   },
-      -- },
     },
   },
 
-  -- Mapping data with "desc" stored directly by vim.keymap.set().
-  --
-  -- Please use this mappings table to set keyboard mapping since this is the
-  -- lower level configuration and more robust one. (which-key will
-  -- automatically pick-up stored data by this setting.)
-  mappings = {
-    -- first key is the mode
-    n = {
-      -- second key is the lefthand side of the map
-      -- mappings seen under group name "Buffer"
-      ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-      ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = "Pick to close" },
-      ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
-      ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
-      -- quick save
-      -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-
-      -- vim-slime
-      -- To be combined with vim.b.slime_cell_delimiter in ftplugin/*.lua
-      ["<C-c>c"] = { "<Plug>SlimeSendCell", desc = "Slime: send cell" },
-
-      ["<leader>fg"] = { "<cmd>lua require('telescope.builtin').git_files()<cr>", desc = "Search git files" },
-      ["<leader>gf"] = { "<cmd>lua require('telescope.builtin').git_files()<cr>", desc = "Search git files" },
-    },
-    t = {
-      -- setting a mapping to false will disable it
-      -- ["<esc>"] = false,
-    },
-  },
-
-  -- Configure plugins
-  plugins = {
-    init = {
-      -- You can disable default plugins as follows:
-      -- ["goolord/alpha-nvim"] = { disable = true },
-
-      -- You can also add new plugins here as well:
-      -- Add plugins, the packer syntax without the "use"
-      {
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-          require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-          })
-        end
-      },
-      { "EdenEast/nightfox.nvim" },
-      { "khaveesh/vim-fish-syntax" },
-      { "vim-pandoc/vim-pandoc" },
-      {
-        "JuliaEditorSupport/julia-vim",
-        config = function()
-          vim.g.latex_to_unicode_tab = "command"
-          vim.g.latex_to_unicode_auto = 1
-          vim.g.latex_to_unicode_file_types = '.*'
-          vim.g.latex_to_unicode_file_types_blacklist = 'tex'
-          vim.g.julia_indent_align_brackets = 0
-          vim.g.julia_indent_align_funcargs = 0
-        end,
-      },
-      {
-        "lervag/vimtex",
-        ft = "tex",
-        config = function()
-          vim.g.vimtex_view_method = "general"
-          vim.g.vimtex_view_general_viewer = "okular"
-          vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-          vim.g.vimtex_quickfix_open_on_warning = 0
-          vim.g.vimtex_fold_manual = 1
-        end,
-      },
-      {
-        "jpalardy/vim-slime",
-        config = function()
-          vim.g.slime_target = "tmux"
-          vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
-          vim.g.slime_paste_file = vim.call("tempname")
-          vim.g.slime_bracketed_paste = 1
-        end,
-      },
-      {
-        "andymass/vim-matchup",
-        -- setup = function()
-        --   vim.g.matchup_matchparen_offscreen = { method = "popup" }
-        --   vim.builtin.treesitter.matchup.enable = true
-        -- end,
-      },
-      {
-          "TimUntersberger/neogit",
-          requires = {
-              "nvim-lua/plenary.nvim",
-              "sindrets/diffview.nvim",
-          },
-          config = function()
-              require("neogit").setup { 
-                  integrations = {
-                      diffview = true,
-                  },
-               }
-          end,
-      },
-
-      -- { "andweeb/presence.nvim" },
-      -- {
-      --   "ray-x/lsp_signature.nvim",
-      --   event = "BufRead",
-      --   config = function()
-      --     require("lsp_signature").setup()
-      --   end,
-      -- },
-
-      -- We also support a key value style plugin definition similar to NvChad:
-      -- ["ray-x/lsp_signature.nvim"] = {
-      --   event = "BufRead",
-      --   config = function()
-      --     require("lsp_signature").setup()
-      --   end,
-      -- },
-    },
-    -- All other entries override the require("<key>").setup({...}) call for default plugins
-    ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
-      -- config variable is the default configuration table for the setup function call
-      -- local null_ls = require "null-ls"
-
-      -- Check supported formatters and linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
-        -- Set a formatter
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.formatting.prettier,
-      }
-      return config -- return final config table
-    end,
-
-    treesitter = { -- overrides `require("treesitter").setup(...)`
-      ensure_installed = { "lua" },
-    },
-
-    -- use mason-lspconfig to configure LSP installations
-    ["mason-lspconfig"] = { -- overrides `require("mason-lspconfig").setup(...)`
-      ensure_installed = {
-        -- "sumneko_lua",
-        "bashls",
-        -- "texlab",
-      },
-    },
-    -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-    ["mason-null-ls"] = { -- overrides `require("mason-null-ls").setup(...)`
-      -- ensure_installed = { "prettier", "stylua" },
-    },
-  },
-
-  -- LuaSnip Options
-  luasnip = {
-    -- Extend filetypes
-    filetype_extend = {
-      -- javascript = { "javascriptreact" },
-    },
-    -- Configure luasnip loaders (vscode, lua, and/or snipmate)
-    vscode = {
-      -- Add paths for including more VS Code style snippets in luasnip
-      -- This will find directories including ~/.config/{nvim,astronvim}/lua/user/snippets
-      paths = vim.api.nvim_get_runtime_file("lua/user/snippets", true),
-    },
-  },
-
-  -- CMP Source Priorities
-  -- modify here the priorities of default cmp sources
-  -- higher value == higher priority
-  -- The value can also be set to a boolean for disabling default sources:
-  -- false == disabled
-  -- true == 1000
-  cmp = {
-    source_priority = {
-      nvim_lsp = 1000,
-      luasnip = 750,
-      buffer = 500,
-      path = 250,
-    },
-  },
-
-  -- Modify which-key registration (Use this with mappings table in the above.)
-  ["which-key"] = {
-    -- Add bindings which show up as group name
-    register = {
-      -- first key is the mode, n == normal mode
-      n = {
-        -- second key is the prefix, <leader> prefixes
-        ["<leader>"] = {
-          -- third key is the key to bring up next level and its displayed
-          -- group name in which-key top level menu
-          ["b"] = { name = "Buffer" },
-        },
+  -- Configure require("lazy").setup() options
+  lazy = {
+    defaults = { lazy = true },
+    performance = {
+      rtp = {
+        -- customize default disabled vim plugins
+        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
       },
     },
   },
@@ -430,23 +104,6 @@ local config = {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    local goto_remembered_position = function()
-      local buffer = 0 -- corresponds to current buffer
-      local pos = vim.api.nvim_buf_get_mark(buffer, '"') -- pos = (row, col)
-      local line = pos[1]
-      local nlines = vim.api.nvim_buf_line_count(buffer)
-      if line > 1 and line < nlines then
-        local window = 0 -- current window
-        vim.api.nvim_win_set_cursor(window, pos)
-      end
-      return nil
-    end
-    vim.api.nvim_create_autocmd(
-      "BufRead", {
-      desc = "remember last position in file",
-      callback = goto_remembered_position,
-    }
-    )
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
@@ -461,5 +118,3 @@ local config = {
     -- }
   end,
 }
-
-return config
---
+++ lua/user/plugins/user.lua
@@ -0,0 +1,84 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  "EdenEast/nightfox.nvim",
+
+  {
+    "khaveesh/vim-fish-syntax",
+    ft = "fish",  -- only gets loaded when a fish file is opened
+  },
+
+  {
+    "vim-pandoc/vim-pandoc",
+    ft = "markdown",
+  },
+
+  {
+    "JuliaEditorSupport/julia-vim",
+    event = "BufRead",  -- always enable, to be able to use latex-to-unicode in non-Julia files
+    config = function()
+      vim.g.latex_to_unicode_tab = "command"
+      vim.g.latex_to_unicode_auto = 1
+      vim.g.latex_to_unicode_file_types = '.*'
+      vim.g.latex_to_unicode_file_types_blacklist = 'tex'
+      vim.g.julia_indent_align_brackets = 0
+      vim.g.julia_indent_align_funcargs = 0
+    end,
+  },
+
+  {
+    "lervag/vimtex",
+    ft = "tex",
+    config = function()
+      vim.g.vimtex_view_method = "general"
+      vim.g.vimtex_view_general_viewer = "okular"
+      vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+      vim.g.vimtex_quickfix_open_on_warning = 0
+      vim.g.vimtex_fold_manual = 1
+    end,
+  },
+
+  {
+    "jpalardy/vim-slime",
+    event = "BufRead",
+    config = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+      vim.g.slime_paste_file = vim.call("tempname")
+      vim.g.slime_bracketed_paste = 1
+    end,
+  },
+
+  {
+    "kylechui/nvim-surround",
+    event = "BufRead",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+  -- {
+  --   "TimUntersberger/neogit",
+  --   requires = {
+  --     "nvim-lua/plenary.nvim",
+  --     "sindrets/diffview.nvim",
+  --   },
+  --   config = function()
+  --     require("neogit").setup { 
+  --       integrations = {
+  --         diffview = true,
+  --       },
+  --     }
+  --   end,
+  -- },
+}
