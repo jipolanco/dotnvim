2e9d17efa01640be65ea888827d3da9a9f14a32f
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-06-05 16:17:24 +0200

    Update julia.lua

--- ftplugin/julia.lua
+++ ftplugin/julia.lua
@@ -1,5 +1,6 @@
 vim.opt.shiftwidth = 4
 vim.g.julia_set_indentation = 4
+vim.opt.textwidth = 92
 
 -- This is compatible with the Julia VSCode plugin:
 -- https://www.julia-vscode.org/docs/stable/userguide/runningcode/#Julia:-Execute-Code-Cell-in-REPL
