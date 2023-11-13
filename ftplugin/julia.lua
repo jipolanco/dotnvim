2e598dc9c21275ec65bbfbc06e5012589cfeba2d
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-11-13 13:31:03 +0100

    Update slime_cell_delimiter

--- ftplugin/julia.lua
+++ ftplugin/julia.lua
@@ -4,4 +4,4 @@ vim.opt.textwidth = 92
 
 -- This is compatible with the Julia VSCode plugin:
 -- https://www.julia-vscode.org/docs/stable/userguide/runningcode/#Julia:-Execute-Code-Cell-in-REPL
-vim.b.slime_cell_delimiter = "##"
+vim.b.slime_cell_delimiter = "^\\s*##"
