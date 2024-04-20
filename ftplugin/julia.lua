90d5e13aaef420b88812fb6d437e9b9b2b0aff79
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-04-20 10:15:32 +0200

    Delete old files

--- ftplugin/julia.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.shiftwidth = 4
-vim.g.julia_set_indentation = 4
-vim.opt.textwidth = 92
-
--- This is compatible with the Julia VSCode plugin:
--- https://www.julia-vscode.org/docs/stable/userguide/runningcode/#Julia:-Execute-Code-Cell-in-REPL
-vim.b.slime_cell_delimiter = "^\\s*##"
--- ftplugin/python.lua
+++
@@ -1,2 +0,0 @@
--- This is compatible with Spyder
-vim.b.slime_cell_delimiter = "#%%"
